d83f573894745ea58e2da8e916ff42550b6ef33e
Frances Belleza <francesanne.belleza@sjsu.edu>
2025-11-26 22:54:27 -0800

    added model_classes.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import math
+from model_classes import Planet
 pygame.init()
 
 #sets up planet window
@@ -7,14 +8,22 @@ WIDTH, HEIGHT = 800,800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("planetSim")
 
+WHITE = (255, 255, 255)
+
+
+
 #infinite loop that runs throughout
 #the entire simulation to keep running
 # ---- EVENT LOOP ----
 def main():
     run = True
+    clock = pygame.time.Clock()
 
     while run:
-        # this for loop gets different events happening in the sim
+        clock.tick(60) #60 is fps
+        #pygame.display.update()
+
+        # this for loop is for different events happening in the sim
         # we only care about the user clicking the x
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
---
+++ model_classes.py
@@ -0,0 +1,17 @@
+class Planet:
+    # austronomical units, distance from earth to sun
+    AU =
+
+    def __init__(self, x, y, radius, color, mass):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.mass = mass
+
+        self.orbit = []
+        self.sun = False
+        self.distance_to_sun = 0
+
+        self.x_vel = 0
+        self.y_vel = 0
